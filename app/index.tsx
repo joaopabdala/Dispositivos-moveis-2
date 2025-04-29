22f5920d2e2574ce1829b46add7f1c33da820c62
João Paulo Abdala Bohaczk <114778253+joaopabdala@users.noreply.github.com>
2025-04-29 09:01:29 -0300

    remade login and register screen with react-native-elements components (#6)

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,8 @@
+import { Icon, Input, Text } from "@rneui/base";
+import { Button } from "@rneui/themed";
 import { Link, useRouter } from "expo-router";
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import { useState } from "react";
+import { TextInput, View } from "react-native";
 
 export default function IndexScreen() {
   const router = useRouter();
@@ -12,17 +15,48 @@ export default function IndexScreen() {
     router.push("/(auth)/register");
   };
 
+  const [passwordSecure, setPasswordSecure] = useState(true);
+
+  const handlePasswordSecure = () => {
+    setPasswordSecure(!passwordSecure);
+  };
+
   return (
     <View className="flex-1 bg-white justify-center p-12">
-      <TextInput
-        placeholder="Email"
-        className="h-12 border border-gray-300 rounded-lg mb-3 px-3"
-      />
-      <TextInput
-        placeholder="Password"
-        secureTextEntry
-        className="h-12 border border-gray-300 rounded-lg mb-3 px-3"
-      />
+      <Text h1 className="mb-2 text-center">
+        Login
+      </Text>
+      <View className="flex-row items-center h-12 border border-gray-300 rounded-lg mb-3 px-3 bg-white">
+        <Icon type="MaterialCommunityIcons" name="email" />
+        <TextInput
+          placeholder="Email"
+          className="flex-1 ml-2 text-base"
+          placeholderTextColor="#888"
+        />
+      </View>
+
+      <View className="flex-row items-center h-12 border border-gray-300 rounded-lg mb-3 px-3 bg-white">
+        <Icon
+          type="MaterialCommunityIcons"
+          name="lock"
+          color="#888"
+          size={20}
+        />
+        <TextInput
+          placeholder="Password"
+          secureTextEntry={passwordSecure}
+          className="flex-1 ml-2 text-base text-black"
+          placeholderTextColor="#888"
+        />
+        <Icon
+          type="ant-design"
+          name={passwordSecure ? "eye" : "eyeo"}
+          onPress={handlePasswordSecure}
+          size={24}
+          color="black"
+        />
+      </View>
+
       <View className="mt-2 mb-4">
         <Button title="Login" onPress={handleLogin} />
       </View>
--- app/register.tsx
+++ app/register.tsx
@@ -1,5 +1,7 @@
-import { Link, useRouter } from "expo-router";
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import { Icon, Text } from "@rneui/base";
+import { useRouter } from "expo-router";
+import { useState } from "react";
+import { Button, TextInput, View } from "react-native";
 
 export default function IndexScreen() {
   const router = useRouter();
@@ -13,20 +15,52 @@ export default function IndexScreen() {
     router.push("/");
   };
 
+  const [passwordSecure, setPasswordSecure] = useState(true);
+  
+    const handlePasswordSecure = () => {
+      setPasswordSecure(!passwordSecure);
+    };
+
   return (
     <View className="flex-1 bg-white justify-center p-12">
+    <Text h1 className="mb-2 text-center">
+      Register
+    </Text>
+    <View className="flex-row items-center h-12 border border-gray-300 rounded-lg mb-3 px-3 bg-white">
+      <Icon type="MaterialCommunityIcons" name="email" />
       <TextInput
         placeholder="Email"
-        className="h-12 border border-gray-300 rounded-lg mb-3 px-3"
+        className="flex-1 ml-2 text-base"
+        placeholderTextColor="#888"
+      />
+    </View>
+
+    <View className="flex-row items-center h-12 border border-gray-300 rounded-lg mb-3 px-3 bg-white">
+      <Icon
+        type="MaterialCommunityIcons"
+        name="lock"
+        color="#888"
+        size={20}
       />
       <TextInput
         placeholder="Password"
-        className="h-12 border border-gray-300 rounded-lg mb-3 px-3"
-        secureTextEntry
+        secureTextEntry={passwordSecure}
+        className="flex-1 ml-2 text-base text-black"
+        placeholderTextColor="#888"
       />
-      <View className="mt-2 mb-4">
-        <Button title="Registar" onPress={handleRegister} />
-      </View>
+      <Icon
+        type="ant-design"
+        name={passwordSecure ? "eye" : "eyeo"}
+        onPress={handlePasswordSecure}
+        size={24}
+        color="black"
+      />
     </View>
+
+    <View className="mt-2 mb-4">
+      <Button title="Registrar" onPress={handleRegister} />
+    </View>
+
+  </View>
   );
 }
